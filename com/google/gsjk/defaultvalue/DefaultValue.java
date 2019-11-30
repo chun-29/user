c2064f2fe2b1f44f95ddccf5ab2673d664d34b6a
Your Name <you@example.com>
2019-11-30 20:55:12 +0800

    created a new DefaultValue class

---
+++ com/google/gsjk/defaultvalue/DefaultValue.java
@@ -0,0 +1,14 @@
+package com.google.gsjk.defaultvalue;
+
+/**
+ * @program: user
+ * @Description: testing purpose
+ * @author: chun
+ * @date: 2019/11/25 下午
+ */
+public class DefaultValue {
+    public final static String USERNAME = "test";
+    public final static String PASSWORD = "123456";
+    public final static String FILEPATH = "./resource/";
+    public final static String FILETYPE = ".txt";
+}
